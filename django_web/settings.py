2fdae1bfa1c399480dfcf40a9a5b94c0faacb714
xiejia <xiejia@cnnic.cn>
2019-01-07 14:05:27 +0800

    fix 'CSRFCheck' object has no attribute 'process_request'

--- django_web/settings.py
+++ django_web/settings.py
@@ -103,6 +103,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": []
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
